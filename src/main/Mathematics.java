17762e90189843484cd05093517a1f487b9841bc
南图 <vinthony@gmail.com>
2014-12-11 01:28:19 +0800

    大数减法完成

--- src/main/Mathematics.java
+++ src/main/Mathematics.java
@@ -12,10 +12,11 @@ public class Mathematics {
     }
 
     //加法运算
-    public  String Add(String s1, String s2, String ret)
+    public  String Add(String s1, String s2)
     {
-        int carry = 0;  //进位
-        if(s1.equals("")||s2.equals("")) return "";
+        if(s1.equals("0")) return s2;
+        if(s2.equals("0")) return s1;
+        if(s1.equals("")||s2.equals("")) return s1+s2;
         if((!isNum(s1))||(!isNum(s2))) return "error";
 
 //        简化操作
@@ -24,15 +25,16 @@ public class Mathematics {
         String longer = ns1.length()>ns2.length() ? ns1 : ns2;
         String shorter = ns1.length()>ns2.length() ? ns2 : ns1;
         String num = "";//保存数据的位置
+        int carry = 0;  //进位
         int n = shorter.length();
         int x = 0;//进位
         int y = 0;//剩余
 //      如果两个负数，去掉符号之后相加
         if(isNegative(ns1)&&isNegative(ns2))
-            Add(removeSign(ns1),removeSign(ns2),null);
+            Add(removeSign(ns1),removeSign(ns2));
 //        如果存在一个负数，当减法处理
-        if(isNegative(ns2)||isNegative(ns2))
-            Sub(ns1,ns2,null);
+        if(isNegative(ns1)||isNegative(ns2))
+            Sub(ns1,ns2);
 
         String prev = longer.substring(0,longer.length()-shorter.length());
         String longer2 = longer.substring(prev.length());
@@ -49,30 +51,91 @@ public class Mathematics {
             n--;
         }
         if(carry>0)
-            return Add(prev,Integer.toString(x),null)+num;
+            return Add(prev,Integer.toString(x))+num;
         else
             return prev+num;
     }
 
 
     //减法运算
-    public  boolean Sub(String s1, String s2, String ret) {
+    public  String Sub(String s1, String s2) {
+        if(s1.equals("")||s2.equals("")) return s1+s2;
+        if((!isNum(s1))||(!isNum(s2))) return "error";
         String ns1 = legalify(s1);
         String ns2 = legalify(s2);
+//      -a-b = -(a+b)
+        if(isNegative(ns1)&&(!isNegative(ns2))) return "-"+Add(removeSign(s1),removeSign(s2));
+//        a-(-b) = a+b
+        if(isNegative(ns2)) return Add(s1,removeSign(s2));
+//       -a-(-b) = b-a = -(a-b)
+        if(isNegative(ns1)&&isNegative(ns2)) return Sub(s2, s1);
 
-        return true;
+//        处理 a-b a>b ? a< b
+        String bigger = max(ns1,ns2);
+        String smaller = min(ns1, ns2);
+        String prefix = bigger.equals(ns1) ? "" :"-";
+        String prev = bigger.substring(0,bigger.length()-smaller.length());
+        String bigger2 = bigger.substring(prev.length());
+        int n = smaller.length();
+        int borrowed = 0;
+        int num;
+        String str = "";
+        while(n>0){
+            int temp = getInt(bigger2.charAt(n-1)) - getInt(smaller.charAt(n-1))-borrowed;
+            if(temp<0){//如果不够减
+                num = temp + 10;
+                borrowed++;
+            }else{
+                num = temp;
+                borrowed = 0;
+            }
+            str = num+str;
+            n--;
+        }
+
+        if(borrowed!=0)
+            return legalify(prefix+Sub(prev,Integer.toString(borrowed))+str);
+        else
+            return legalify(prefix+str);
     }
 
     //乘法运算
-    public  boolean Mul(String s1, String s2, String ret)
+    public  String Mul(String s1, String s2)
     {
-        return true;
+        if((!isNum(s1))||(!isNum(s2))) return "error";
+        String ns1 = legalify(s1);
+        String ns2 = legalify(s2);
+        String prefix = (isNegative(ns1)&&isNegative(ns2))||(!isNegative(ns1)&&!isNegative(ns2)) ? "" : "-";
+        String n1 = removeSign(ns1);
+        String n2 = removeSign(ns2);
+        String finalStr = "";
+        int temp;
+        int m = 0;
+        int n;
+//        log(n1);
+//        log(n2);
+//        转换成一位数乘法 取集合
+        for (int i = n1.length()-1; i >= 0 ;i--){
+            String str = "";
+            for (int j =n2.length()-1 ;j >= 0;j--){
+//                log(n2.charAt(j-1));
+//                log(n1.charAt(i-1));
+                temp = getInt(n2.charAt(j))*getInt(n1.charAt(i))+m;
+                 m = temp/10;//进位
+                 n = temp%10;//本位
+                str = n + str;
+            }
+            finalStr = Add(str,finalStr);
+        }
+        log("f"+finalStr);
+
+        return prefix+finalStr;
     }
 
     //除法运算
-    public  boolean Div(String s1, String s2, String ret)
+    public  String Div(String s1, String s2)
     {
-        return true;
+        return "2";
     }
 
     /*
@@ -141,5 +204,16 @@ public class Mathematics {
     }
     private void log(int c){
         System.out.println(c);
+    }
+    private String max(String s1,String s2){
+       if(s1.length() != s2.length()) return s1.length()>s2.length()? s1 : s2;
+       for (int i = 0; i<s1.length();i++){
+           if(getInt(s1.charAt(i))-getInt(s2.charAt(i))!=0)
+               return getInt(s1.charAt(i))>getInt(s2.charAt(i))? s1 : s2;
+       }
+        return s1;
+    }
+    private String min(String s1,String s2){
+        return max(s1,s2).equals(s1) ? s2 : s1;
     }
 }
